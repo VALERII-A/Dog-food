f28a56a22d87f3e7575f0c9deba50266d90d3d4e
VALERII-A <saligasaliga100@gmail.com>
2023-03-02 17:42:53 +0500

    Была реализована функция когда при закрытии модалки по клику на оверлей,пользователь перемещался на роут с которого была открыта модалка

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,19 +1,22 @@
 import cn from "classnames";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import './index.css'
 
 
 export const Modal = ({ children, activeModal, setActiveModal }) => {
 
   const navigate = useNavigate();
+  const location = useLocation();
+  const Path = location.state?.backgroundLocation;
+  console.log(Path.pathname);
 
 useEffect(()=>{
  
 },[]);
 
   return (
-      <div className={cn('modal', {['active']: activeModal})} onClick={()=>{setActiveModal(false) ; navigate('/') }}>
+      <div className={cn('modal', {['active']: activeModal})} onClick={()=>{setActiveModal(false) ; navigate(Path) }}>
       <div className={cn('modal_content', {['active']: activeModal})} onClick={e=> e.stopPropagation()}>{children}</div>
     </div>
   );
