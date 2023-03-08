b84bf88e550d59b7625a62dc1604e15a5ea60ab9
VALERII-A <saligasaliga100@gmail.com>
2023-03-08 13:11:36 +0500

    Добавил отображение аватаров пользователей

--- src/components/Product/product.jsx
+++ src/components/Product/product.jsx
@@ -53,9 +53,13 @@ export const Product = ({
   };
 
   const getUser = (id) => {
-    if (!users.length) return 'User';
+    if (!users.length) return "User";
     const user = users.find((el) => el._id === id);
-    return user?.name ?? 'User';
+    console.log({ user });
+    if (user.avatar.includes('default-image')) {
+      return {...user, avatar : 'https://media.istockphoto.com/id/1300845620/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C-icon-flat-%D0%B8%D0%B7%D0%BE%D0%BB%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC-%D1%84%D0%BE%D0%BD%D0%B5-%D1%81%D0%B8%D0%BC%D0%B2%D0%BE%D0%BB-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F-%D0%B8%D0%BB%D0%BB%D1%8E%D1%81%D1%82%D1%80%D0%B0%D1%86%D0%B8%D1%8F-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B0.jpg?s=612x612&w=0&k=20&c=Po5TTi0yw6lM7qz6yay5vUbUBy3kAEWrpQmDaUMWnek='}
+    }
+    return user;
   };
 
   const options = {
@@ -252,7 +256,8 @@ export const Product = ({
           <div key={e.created_at} className={s.review}>
             <div className={s.review__author}>
               <div className={s.review__info}>
-                <span>{getUser(e.author)} </span>
+              <img src={getUser(e.author)?.avatar} className={s.review__avatar} alt="avatar" />
+                <span>{getUser(e.author)?.name ?? 'User'} </span>
                 <span className={s.review__date}>
                   {new Date(e.created_at).toLocaleString('ru', options)}
                 </span>
