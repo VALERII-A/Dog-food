2c5e8e9bee898472a5061b07eeb997f6004f6124
VALERII-A <saligasaliga100@gmail.com>
2023-03-13 16:16:27 +0500

    Added back button

--- src/pages/favorites/favorites.jsx
+++ src/pages/favorites/favorites.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import CardList from "../../components/CardList/card-list";
 import { CardContext } from "../../context/cardContext";
 import { UserContext } from "../../context/userContext";
@@ -7,12 +8,13 @@ export const Favorite = ({ currentUser })=> {
 
  const {favorites  } = useContext(CardContext);
  const { handleProductLike } = useContext(UserContext);
+ const navigate = useNavigate();
 
     return (
         <>
-        {/* <Link to='/'>
-        <button>Назад</button>
-        </Link> */}
+         <span className='profile__back' onClick={()=>{navigate(-1)}}>
+                {'< Назад'}
+            </span>
           <div>
             <h3>Понравилось</h3>
            <div className='content__cards'>
